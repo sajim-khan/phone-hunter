c6e5ae8ac30f4f6ea2b4a67f4d32cec46061813e
sajim-khan <shossain1427@gmail.com>
2023-02-27 16:32:49 +0600

    added Toggle no phone found message based on search result

--- js/app.js
+++ js/app.js
@@ -2,14 +2,27 @@ const loadPhone = async(searchText) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data);
+    //console.log(data);
+    displayPhones(data.data);
 }
-
 const displayPhones = phones =>{
-
     const phoneContainer = document.getElementById('phone-container');
-    phoneContainer.textContent = '';
-        phones.data.forEach(phone =>{
+        phoneContainer.textContent = '';
+        console.log(phones);
+        
+        // display phones by slicing 
+        phones = phones.slice(0, 10)
+        
+        // Display no phone found
+        const noPhoneFound = document.getElementById('no-found-message');
+        if(phones.length === 0) {
+            noPhoneFound.classList.remove('d-none') 
+        } else{
+            noPhoneFound.classList.add('d-none') 
+        }
+        
+        // Display all phones
+        phones.forEach(phone =>{
         const phoneDiv = document.createElement('div')
         phoneDiv.classList.add('col')
         phoneDiv.innerHTML = `
@@ -17,7 +30,7 @@ const displayPhones = phones =>{
                 <img src="${phone.image}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h3 class="my-4">Brand: ${phone.brand}</h3>
-                    <h5 class="card-title">Phone Name: ${phone.phone_name}</h5>
+                    <h5 class="card-title">Name: ${phone.phone_name}</h5>
                 </div>
             </div>
         `;
@@ -31,5 +44,5 @@ document.getElementById('btn-search').addEventListener('click', function(){
     loadPhone(searchText)
     
 })
-
 loadPhone();
+
